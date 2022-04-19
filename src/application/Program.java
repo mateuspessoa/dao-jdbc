d286e118128fb1668b581fe8ddc4a02eac5926d6
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-04-19 14:42:49 -0300

    Implementação do Delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -42,6 +45,14 @@ public class Program {
 		seller.setName("Marta Waine");
 		sellerDao.update(seller);
 		System.out.println("Update Completado!");
+		
+		System.out.println("\n==== TESTE 6: Seller Delete ====");
+		System.out.println("Digite um id para remover: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Deletado com sucesso!");
+		
+		sc.close();
 	}
 
 }
